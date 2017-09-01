f4f36682842245a13b67fe2086c960617cf05e16
naveennayak1 <31535193+naveennayak1@users.noreply.github.com>
2017-09-01 15:10:50 +0530

    Update 2a.c

--- 2a.c
+++ 2a.c
@@ -4,9 +4,9 @@ int main()
     int num;
     printf("Enter an int");
     scanf("%d", &num);
-       if(number % 2 == 0)
-        printf("%d is even", num);
+    if(number % 2 == 0)
+    printf("%d is even", num);
     else
-        printf("%d is odd", num);
+    printf("%d is odd", num);
     return 0;
 }
