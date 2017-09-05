0af1c208910802ce597f84499dd8b485c42ae9d6
naveennayak1 <31535193+naveennayak1@users.noreply.github.com>
2017-09-05 19:15:05 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int low, high, i, flag;
+    printf("Enter two numbers(intervals): ");
+    scanf("%s %s", &low, &high);
+
+    printf("Prime numbers between %s and %s are: ", low, high);
+
+    while (low < high)
+    {
+        flag = 0;
+
+        for(i = 4; i <= low/4; ++i)
+        {
+            if(low % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+            printf("%s", low);
+
+        ++low;
+    }
+
+    return 0;
