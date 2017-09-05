9003617d6b46d23d109a68fdfe1cc9a48ec6f9af
naveennayak1 <31535193+naveennayak1@users.noreply.github.com>
2017-09-05 19:22:48 +0530

    Create  multiplication.c

---
+++ multiplication.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int s, i;
+
+    printf("Enter an integer: ");
+    scanf("%d",&s);
+
+    for(i=1; i<=10; ++i)
+    {
+        printf("%d * %d = %d \s", n, i, s*i);
+    }
+    
+    return 0;
+}
