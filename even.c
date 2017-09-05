f1dbf5dccc46e83435463a3b8960933f155aebee
naveennayak1 <31535193+naveennayak1@users.noreply.github.com>
2017-09-05 18:34:06 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2, n, j, p, y;
+    printf("number of intervals: ");
+    scanf("%d", &y);
+    for(j=1; j<=y; j++)
+    {
+        scanf("%d", &n1);
+        for(i=1;i<=n1; i++)
+        {
+            p=i%2;
+            if(p==0)
+                printf(" %d", n);     
+        }
+        return 0;
+    } 
+}
