2689d5c02de0024f4b40ea6b0472b4a48d23324e
naveennayak1 <31535193+naveennayak1@users.noreply.github.com>
2017-09-05 19:27:08 +0530

    Create number is prime.c

---
+++ number is prime.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int s, i, flag = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d",&s);
+
+    for(i=2; i<=s/2; ++i)
+    {
+        // condition for nonprime number
+        if(s%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+
+    if (flag==0)
+        printf("%d is a prime number.",s);
+    else
+        printf("%d is not a prime number.",s);
+    
+    return 0;
+}pr
