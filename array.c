d70d2b6513558d36106c911c812404fb50e3d2a8
naveen <31535193+naveennayak1@users.noreply.github.com>
2018-01-10 10:41:13 +0000

    Create array.c

---
+++ array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int array[10];
+int i, N, keynum, found=0;
+clrscr();
+printf("Enter the value of N\n");
+scanf("%d",&N);
+printf("Enter the elements one by one\n");
+for(i=0; i<N ; i++)
+{
+scanf("%d",&array[i]);
+}
+printf("Input array is\n");
+for(i=0; i<N ; i++)
+{
+printf("%d\n",array[i]);
+}
+printf("Enter the element to be searched\n");
+scanf("%d", &keynum);
