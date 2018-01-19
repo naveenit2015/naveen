696e08cc6ef78ef256173cf7dbf65d9c09a3172f
naveen <31535193+naveennayak1@users.noreply.github.com>
2018-01-19 10:47:28 +0000

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n,a[100];
+printf("Input the number of elements to store in the array :");
+scanf("%d",&n);
+printf("Input %d number of elements in the array :\n",n);
+for(i=0;i<n;i++)
+{
+printf("element - %d : ",i);
+scanf("%d",&a[i]);
+}
+printf("\nThe values store into the array are:\n");
+for(i=0;i<n;i++)
+{
+printf("% 5d",a[i]);
+}
+printf("\nThe values store in array are:\n);
+getch();
+}
