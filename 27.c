c61fe506ba01869add1e6d0806dbf7446e2d659f
naveen <31535193+naveennayak1@users.noreply.github.com>
2018-01-19 11:13:46 +0000

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s;
+printf("Enter the value of s");
+scanf("%d",&s);
+if(s>=0&&s<=9)
+{
+ printf("yes");
+ }
+ else
+ {
+ printf("no");
+ }
+ getch();
+}
