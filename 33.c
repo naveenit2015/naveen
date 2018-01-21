0ccaa8929d6bc9580dfb628672e96deff4e30ddb
naveen <31535193+naveennayak1@users.noreply.github.com>
2018-01-21 13:45:59 +0530

    Create 33.c

---
+++ 33.c
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<conio.h>
+#include <algorithm>
+using namespace std;
+ 
+string removeSpaces(string str)
+{
+return str;
+}
+int main() 
+{
+    
+str = removeSpaces(str);
+cout << str;
+return 0;
+}
+ 
