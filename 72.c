112aa624228a9ee776f5da9c6df159769d585e71
naveen <31535193+naveennayak1@users.noreply.github.com>
2018-01-21 15:49:09 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[10];
+int l,i;
+gets(a);
+l=strlen(a);
+for(i=0;i<l;i++)
+{
+if(a[i]=='a'&&a[i]=='e'&&a[i]=='i'&&a[i]=='o'&&a[i]=='u')
+{
+puts(yes);
+}
