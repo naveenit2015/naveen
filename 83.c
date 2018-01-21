feeb7a2bffe6cbc3842d7da668923901c2b66fb3
naveen <31535193+naveennayak1@users.noreply.github.com>
2018-01-21 16:04:21 +0530

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+
+{
+long int a,b,div,mod,n;
+scanf("%d",&n);
+switch(n)
+{
+	case 1:
+	scanf("%ld / %ld",&a,&b);
+	printf("%ld",a/b);
+	break;
+	case 2:
+	scanf("%ld % %ld",&a,&b);
+
+	printf("%ld",(a%b));
+
+	break;
+}
