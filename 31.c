e82a041871da8c75d4bc30813be3f5364a0831d4
naveen <31535193+naveennayak1@users.noreply.github.com>
2018-01-21 13:42:16 +0530

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int count_words=0,i;
+ int count_char=0;
+ char str[20];
+ printf("Enter string : ");
+ gets(str);
+ for(i=0; str[i]!=NULL; i++)
+ {
+ printf("\nNumber of characters in string : %d",count_char);
+ printf("\nNumber of words in string : % d",count_words+1); 
+ getch();
+ return 0;
+ }
+}
