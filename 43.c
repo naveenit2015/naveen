cde971142331769ad4d2cbbd8c8c116dd7fb128a
naveen <31535193+naveennayak1@users.noreply.github.com>
2018-01-21 14:05:50 +0530

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char a[50],b[50],i,j;
+	scanf("%s",&a);
+	scanf("%s",&b);
+	for(i=0;a[i]!='\0';++i)
+	{
+		for(i=0;b[j]!='\0';++j,++i)
+		{
+			a[i]=b[j];
+		}
+		a[i]='\0';
+	}
+	printf("%s",a);
+  return 0;
