7e67325d5cf06b109fd5af511693978e8b71f808
naveen <31535193+naveennayak1@users.noreply.github.com>
2018-01-24 16:47:42 +0530

    Create 92.c

---
+++ 92.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+int b;
+int c;
+c=a*b;
+printf("enter the value of a");
+scanf("%d",&a);
+printf("enter the value of b");
+scanf("%d",&b);
+printf("%d",&c);
+return(0);
+}
